67817d3471c3282962e1ac4bc00ccc3b5c56b9ff
asegur01 <adan.segura@prgx.com>
2023-11-09 11:42:35 -0600

    Add say_goodbye function

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,10 @@
 def say_hello() -> None:    
     print("Hello World")
 
+def say_goodbye() -> None:
+    print("Goodbye")
+
 if __name__ == '__main__':
     print("\tStarting application...\n")
     say_hello()
+    say_goodbye()
